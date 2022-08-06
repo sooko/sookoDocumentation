130dc3504bd79bbdee53f94e18b97ec6cd7fe785
sooko <hansmotor89@gmail.com>
2022-08-06 23:41:01 +0700

    first doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'sooko_documentation'
+copyright = '2022, Burhan'
+author = 'Burhan'
 
 release = '0.1'
 version = '0.1.0'
